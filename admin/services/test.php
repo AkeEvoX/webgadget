2ff6bcf1df1f25dcdd9109c257a234c124bfecbd
AkeEvoX <svargalok@gmail.com>
2017-10-29 16:09:44 +0700

    test get second time

---
+++ admin/services/test.php
@@ -0,0 +1,10 @@
+<?php
+include("../../lib/common.php");
+
+//$dir =  "../../images/products/123/";
+//mkdir($dir,0777,true);
+//echo "create directory complete.";
+
+echo date("s").gettimeofday()["usec"];
+
+?>
